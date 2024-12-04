7d9726ed24754a39f8e20390754d4bbfe04c9ec6
Pedro Ringuelet <peter.ringuelet@gmail.com>
2024-12-04 14:38:46 -0300

    wspp sound

--- escribania-ringuelet/app/components/WhatsAppButton.tsx
+++ escribania-ringuelet/app/components/WhatsAppButton.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
-import { useEffect, useState, useRef } from "react"
+import { useEffect, useRef } from "react"
 import { motion } from "framer-motion"
 
 export function WhatsAppButton() {
