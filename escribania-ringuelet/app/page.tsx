0f1a220ccfd48ebdca535f3fdabe6533d5277d84
Pedro Ringuelet <peter.ringuelet@gmail.com>
2024-12-03 23:43:12 -0300

    acomodo header

--- escribania-ringuelet/app/page.tsx
+++ escribania-ringuelet/app/page.tsx
@@ -1,4 +1,3 @@
-import { Header } from './components/Header'
 import { Hero } from './components/Hero'
 import { Features } from './components/Features'
 import { Services } from './components/Services'
@@ -9,12 +8,10 @@ import { Clients } from './components/Clients'
 import { News } from './components/News'
 import { ContactForm } from './components/ContactForm'
 import { WhatsAppButton } from './components/WhatsAppButton'
-import { Footer } from './components/Footer'
 
 export default function HomePage() {
   return (
     <>
-      <Header />
       <Hero />
       <Features />
       <Services />
@@ -25,7 +22,6 @@ export default function HomePage() {
       <News />
       <ContactForm />
       <WhatsAppButton />
-      <Footer />
     </>
   )
 }
