ee97e887b0241285e49a0a7c796df76a13e6eff7
Pedro Ringuelet <peter.ringuelet@gmail.com>
2024-12-05 12:53:05 -0300

    optimizacion

--- escribania-ringuelet/app/layout.tsx
+++ escribania-ringuelet/app/layout.tsx
@@ -19,25 +19,26 @@ export const metadata: Metadata = {
     icon: [
       {
         url: '/favicon.ico',
-        sizes: '32x32',
+        sizes: '48x48',
         type: 'image/x-icon',
       },
       {
-        url: '/favicon.svg',
-        type: 'image/svg+xml',
-      },
-      {
-        url: '/favicon-96x96.png',
-        sizes: '96x96',
+        url: '/favicon-16x16.png',
+        sizes: '16x16',
         type: 'image/png',
       },
       {
-        url: '/icon-192x192.png',
+        url: '/favicon-32x32.png',
+        sizes: '32x32',
+        type: 'image/png',
+      },
+      {
+        url: '/android-chrome-192x192.png',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        url: '/icon-512x512.png',
+        url: '/android-chrome-512x512.png',
         sizes: '512x512',
         type: 'image/png',
       },
