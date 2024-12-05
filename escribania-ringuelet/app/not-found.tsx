b22fb410a5fb8aaa23b0c57a22ed0d516c903c10
Pedro Ringuelet <peter.ringuelet@gmail.com>
2024-12-04 23:51:15 -0300

    ruta random

--- escribania-ringuelet/app/not-found.tsx
+++ escribania-ringuelet/app/not-found.tsx
@@ -1,6 +1,14 @@
-import { redirect } from 'next/navigation'
+'use client'
+
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 
 export default function NotFound() {
-    redirect('/')
+    const router = useRouter()
+
+    useEffect(() => {
+        router.push('/')
+    }, [router])
+
     return null
 }
