64aefca6823f235087cd407fab13c1c5118afa61
Pedro Ringuelet <peter.ringuelet@gmail.com>
2024-12-06 15:55:13 -0300

    fixed

--- escribania-ringuelet/app/components/Hero.tsx
+++ escribania-ringuelet/app/components/Hero.tsx
@@ -7,7 +7,7 @@ import Image from "next/image"
 
 export function Hero() {
   return (
-    <section id="inicio" className="relative h-[100dvh] sm:h-[55vh] md:h-[60vh] lg:h-[70vh] flex items-center">
+    <section id="inicio" className="relative h-[100dvh] sm:h-[55vh] md:h-[60vh] lg:h-[60vh] flex items-center">
       <div className="container mx-auto px-4 flex flex-col justify-center">
         <div className="absolute inset-0 z-0">
           <Image
