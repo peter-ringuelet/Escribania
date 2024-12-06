962b9c0ea947b78a7494fdb4bc0cb060c795593f
Pedro Ringuelet <peter.ringuelet@gmail.com>
2024-12-06 14:36:52 -0300

    transition

--- escribania-ringuelet/app/components/Timeline.tsx
+++ escribania-ringuelet/app/components/Timeline.tsx
@@ -63,7 +63,7 @@ export function Timeline() {
               key={event.year}
               initial={{ opacity: 0, x: 50 }}
               animate={isInView ? { opacity: 1, x: 0 } : { opacity: 0, x: 50 }}
-              transition={{ duration: 0.5, delay: 0.7 + index * 0.2 }}
+              transition={{ duration: 0.5, delay: 0.8 + index * 0.3 }}
             >
               <TimelineItem {...event} />
             </motion.div>
