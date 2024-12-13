25df76b08ee4c1ea668022d73b4b3d207fb37ceb
Pedro Ringuelet <peter.ringuelet@gmail.com>
2024-12-13 15:27:07 -0300

    detalles

--- escribania-ringuelet/app/components/AboutUs.tsx
+++ escribania-ringuelet/app/components/AboutUs.tsx
@@ -104,7 +104,7 @@ export function AboutUs() {
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true }}
-          transition={{ duration: 0.3, delay: 0.5 }}
+          transition={{ duration: 0.3, delay: 0.3 }}
           className="text-2xl font-semibold mb-8 text-center mt-16"
         >
           Nuestro Equipo
